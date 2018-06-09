edfcc37b2361765781ee12f096cc2b71f68025d9
Jeff Wong <jeffmjwong@gmail.com>
2018-06-09 12:07:34 +1000

    Readline on events line and close

--- prompt-person.js
+++ prompt-person.js
@@ -2,4 +2,27 @@ const readline = require('readline');
 
 const rl = readline.createInterface(process.stdin, process.stdout);
 
-rl.question('What is your favourite color? ', answer => console.log(answer));
+const realPerson = {
+  name: '',
+  sayings: []
+};
+
+rl.question('What is your name? ', answer => {
+  realPerson.name = answer;
+  rl.setPrompt(`What would ${realPerson.name} say? `);
+  rl.prompt();
+  rl.on('line', saying => {
+    if (saying.toLowerCase().trim() === 'exit') {
+      rl.close();
+    } else {
+      realPerson.sayings.push(saying.trim());
+      rl.setPrompt(`What else would ${realPerson.name} say? ('exit' to leave) `)
+      rl.prompt();
+    }
+  });
+});
+
+rl.on('close', () => {
+  console.log('%s is a real person that says %j', realPerson.name, realPerson.sayings);
+  process.exit();
+})
